d7a5f163387a11473d0c1ad10d7061299e24bd0a
RituPatel16 <ritupatel162006@gmail.com>
2026-08-18 20:34:41 +0530

    Time: 675 ms (23.74%), Space: 0B (100%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,12 @@
+/* Write your T-SQL query statement below */
+
+with cte as
+(
+    select *,
+           lag(num, 1) over(order by id) as prev1,
+           lag(num, 2) over(order by id) as prev2
+    from Logs
+)
+select distinct num as ConsecutiveNums 
+from cte
+where num = prev1 and num = prev2
