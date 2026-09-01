f0598c81a68d696193d77186f3c56a1a4366b252
RituPatel16 <ritupatel162006@gmail.com>
2026-09-01 12:53:42 +0530

    Time: 358 ms (46.51%), Space: 0B (100%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+select p.product_id,round(COALESCE(sum(p.price * u.units)*1.0 / sum(u.units),0),2) as average_price
+from Prices p
+left join UnitsSold u
+on p.product_id = u.product_id
+and u.purchase_date between p.start_date and p.end_date
+group by p.product_id
+
