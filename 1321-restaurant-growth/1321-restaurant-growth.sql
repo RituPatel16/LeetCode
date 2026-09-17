d13768645a7100fab1d16ba195678497b22c1eaf
RituPatel16 <ritupatel162006@gmail.com>
2026-09-17 10:53:20 +0530

    Time: 389 ms (29.49%), Space: 0B (100%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,21 @@
+WITH daily AS (
+    SELECT
+        visited_on,
+        SUM(amount) AS daily_amount
+    FROM Customer
+    GROUP BY visited_on
+)
+SELECT
+    d1.visited_on,
+    SUM(d2.daily_amount) AS amount,
+    ROUND(
+        CAST(SUM(d2.daily_amount) AS DECIMAL(10,2)) / 7,
+        2
+    ) AS average_amount
+FROM daily d1
+JOIN daily d2
+    ON d2.visited_on BETWEEN DATEADD(day, -6, d1.visited_on)
+                         AND d1.visited_on
+GROUP BY d1.visited_on
+HAVING COUNT(d2.visited_on) = 7
+ORDER BY d1.visited_on;
