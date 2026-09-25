c680208ba0d5f757d861fa599c21569ef7489fe8
RituPatel16 <ritupatel162006@gmail.com>
2026-09-25 13:25:30 +0530

    Time: 1151 ms (71.25%), Space: 0B (100%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,6 @@
+/* Write your T-SQL query statement below */
+select p.project_id, round(avg(cast(experience_years as decimal(10,2))), 2) as average_years 
+from Employee e
+join Project p
+on e.employee_id  = p.employee_id 
+group by p.project_id  
